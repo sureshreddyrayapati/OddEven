d9ee7d755ecdb51b668a8f78f865858b721cca01
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-16 09:49:39 +0530

    First

---
+++ OddEven.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSystem
+{
+    public class OddEven
+    {
+        static int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+        public static void OddNumber()
+        {
+            foreach (int i in numbers)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+        public static void EvenNumber()
+        {
+            foreach (int i in numbers)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NumberSystem;
+Console.WriteLine("Hello, World!");
+OddEven.OddNumber();
+OddEven.EvenNumber();
